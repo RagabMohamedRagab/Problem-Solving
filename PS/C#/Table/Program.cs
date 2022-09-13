2ecaacfb151b8f2ed9e0dcdd442bbd91dee95a7c
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2022-09-13 14:52:16 +0200

    Multiplaction Table

---
+++ PS/C#/Table/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Multiplicationtable
+{
+    class Program{
+        static void Main(string[]args){
+            int N=Int32.Parse(Console.ReadLine());
+            for(int i=1;i<=12;i++){
+                Console.WriteLine($"{N} * {i} = {N*i}");
+            }
+        }
+    }
+}
---
+++ PS/C++/Multiplication table.cpp
@@ -0,0 +1,12 @@
+// Nice Code
+// Complexity Time o(N)
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int N;
+	cin>>N;
+	for(int i=1;i<=12;i++){
+		cout<<N<<" * "<<i<<" = "<<N*i<<endl;
+	}
+	return 0;
+}
