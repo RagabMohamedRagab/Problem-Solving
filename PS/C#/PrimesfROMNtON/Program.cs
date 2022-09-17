a98742b95b1cf5367dd5fbbb77ff4f5b9a272e41
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2022-09-17 20:12:35 +0200

    Primes From 1 To N By C#

---
+++ PS/C#/PrimesfROMNtON/Program.cs
@@ -0,0 +1,25 @@
+﻿// Nice Code
+// Complexity Time O(N^2)
+using System;
+namespace PrimesFrom1ToN
+{
+    class Program{
+        static void Main(string []args){
+         int N=Int32.Parse(Console.ReadLine()); // 10
+         int i=2;
+         while(i<=N){ // 2<=10 3 <=10 4<=10
+            bool Prime=true;  
+            	for(int j=2;j<i;j++){  
+				if(i%j==0){ 
+					Prime=false;
+					break;
+				}
+		}
+            if(Prime){
+                Console.Write($"{i} ");
+            }
+            i++;
+         }
+        }
+    }
+}
