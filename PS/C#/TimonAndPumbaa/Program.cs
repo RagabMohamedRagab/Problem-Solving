25c2992e922a523ab7f86ed1c2f8ab89358514a9
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2022-10-11 12:37:22 +0200

    Update Program.cs

--- PS/C#/TimonAndPumbaa/Program.cs
+++ PS/C#/TimonAndPumbaa/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Happy Code 
+// Complexity Time O(1)
+using System;
+namespace TimonAndPumbaa{
+    class Program{
+        static void Main(string[]args){
+            string []arr=Console.ReadLine().Split(' ');
+            int a=Int32.Parse(arr[0]);
+            int b=Int32.Parse(arr[1]);
+            int res=a-b;
+            if(res>=0){
+                Console.WriteLine($"{res}");
+            }else{
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
