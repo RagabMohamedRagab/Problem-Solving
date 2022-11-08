194692322f1531875e1deb57345ce67c8e690a6a
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2022-11-08 17:55:34 +0200

    Sum Digits

---
+++ PS/C#/SumDidits/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SumDigit{
+    class Program{
+        static void Main(string []args){
+            int N=Int32.Parse(Console.ReadLine());
+            string str=Console.ReadLine();
+            int b=0,sum=0;
+            while(b<N){
+                sum+=(str[b]-'0');
+                b++;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ PS/C++/SumDigit.cpp
@@ -0,0 +1,9 @@
+// Time Complexity O(N)
+// Space Complexity O(N)
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	
+   cout<<'7'-'0'<<endl;
+	return 0;
+}
