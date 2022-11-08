299c6e5c9b2623e943315b150bec9e44932a1af3
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2022-11-08 17:57:25 +0200

    Update SumDigit.cpp

--- PS/C++/SumDigit.cpp
+++ PS/C++/SumDigit.cpp
@@ -3,7 +3,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	
-   cout<<'7'-'0'<<endl;
+	int N,s=0;
+	string str;
+	cin>>N;
+	cin>>str;
+	int a=0;
+	while(a<N){
+		s+=(str[a]-'0');
+		a++;
+	}
+   cout<<sdl;
 	return 0;
 }
