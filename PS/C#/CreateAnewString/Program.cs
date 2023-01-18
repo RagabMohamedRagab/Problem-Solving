18864bd5d04f5fbd439f101731236d4d22a6099f
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-01-18 23:05:32 +0200

    Add files via upload

---
+++ PS/C#/CreateAnewString/Program.cs
@@ -0,0 +1,12 @@
+﻿// Time Complexity O(N^2)
+// Space Complexity O(N)
+using System;
+namespace EvenHatedODD
+{
+    class Program{
+        static void Main(string []args){
+            string []A=Console.ReadLine().Split(' ');
+            Console.WriteLine($"{A[0].Length} {A[1].Length}\n{A[0]} {A[1]}");
+        }
+    }
+}
