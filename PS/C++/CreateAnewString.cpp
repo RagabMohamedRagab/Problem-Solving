04953944ef03cc95e4ac069213b0293993b52bf0
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-01-18 23:04:12 +0200

    Add files via upload

---
+++ PS/C++/CreateAnewString.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	string str1,str2;
+	cin>>str1;
+	cin>>str2;
+	cout<<str1.size()<<" "<<str2.size()<<endl;
+	cout<<str1<<" "<<str2<<endl;
+}
