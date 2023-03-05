cc7562c3474f23316a0395ce711bfc0438b938eb
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-03-06 01:02:17 +0200

    Love Strings Problem Solving

---
+++ PS/C#/ILoveStrings/Program.cs
@@ -0,0 +1,46 @@
+﻿// Happy Code
+// Time Complexity O(N^2)
+// Space Complexity O(N)
+using System;
+namespace ILovestrings
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int N = Int32.Parse(Console.ReadLine());
+            string[] str = null;
+            string newStr=string.Empty;
+            int a = 0;
+            while (++a <= N)
+            {
+                newStr = string.Empty;
+                str = Console.ReadLine().Split(' ');
+                string A = str[0];
+                string B = str[1];
+                int len1 = A.Length;
+                int len2 = B.Length;
+                if (len1 <= len2)
+                {
+                    int i;
+                    for (i = 0; i < len1; i++)
+                    {
+                        newStr += (A[i].ToString() + B[i].ToString());
+                    }
+                    newStr += B.Substring(i);
+                }
+                else
+                {
+                    int i;
+                    for (i = 0; i < len2; i++)
+                    {
+                        newStr += (A[i].ToString() + B[i].ToString());
+                    }
+                    newStr += A.Substring(i);
+                } 
+                Console.WriteLine(newStr);
+            }
+           
+        }
+    }
+}
