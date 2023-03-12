f6010cd63609782de8e584b3c530a95621a042b6
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-03-13 01:05:12 +0200

    DataTypes

---
+++ PS/C#/DataTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿// Happy Code 
+// O(N)
+// O(N) size string * 8 = 10 *8 =80
+using System;
+namespace Name
+{
+   class Program{
+    static void Main(string []args){
+       string [] DataTypes=Console.ReadLine().Split(' ');
+     
+       foreach (var item in DataTypes)
+       {
+        Console.WriteLine(item);
+       }
+
+    }
+   }
+}
