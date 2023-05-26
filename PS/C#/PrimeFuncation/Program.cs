1adaf5590ada6835d185fcfa4a64af29ba52356c
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-05-26 15:54:49 +0300

    Prime Funcation

---
+++ PS/C#/PrimeFuncation/Program.cs
@@ -0,0 +1,40 @@
+﻿// Happy Code 
+// Time Complexity O(N^2)
+// Space Complexity O(1)
+using System;
+namespace PrimeFuncation
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int op = Int32.Parse(Console.ReadLine());
+            for (int a = 1; a <= op; a++)
+            {
+                int N = Int32.Parse(Console.ReadLine());
+                if (N == 1)
+                {
+                    Console.WriteLine("NO");
+                    continue;
+                }
+                if (IsPrime(N))
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+        public static bool IsPrime(int N)
+        {
+            for (int i = 2; i <= Math.Sqrt(N); i++)
+            {
+                if (N % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
