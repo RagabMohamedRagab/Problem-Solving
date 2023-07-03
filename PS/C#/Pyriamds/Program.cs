9173e60344a662f6a4b8e5af102af863b4daf5d7
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-07-03 18:49:35 +0300

    Pyriamds

---
+++ PS/C#/Pyriamds/Program.cs
@@ -0,0 +1,32 @@
+﻿// Happy Code
+// Time Complexity O(N^2)
+// Space Complexity O(1)
+using System;
+namespace PrintEvenIndices
+{
+    internal class Program
+    {
+        public static void Print(int N)
+        {
+            for (int i = 1; i <= N; i++)
+            {
+                for (int a = N; a > i; a--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int b = 1; b < i * 2; b++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+        private static void Main(string[] args)
+        {
+            int N = Int32.Parse(Console.ReadLine());
+            Print(N);
+        }
+    }
+}
---
+++ PS/C++/Pyramid.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Pyramid(int N)
+{
+	for (int i = 1; i <= N; i++)
+	{
+		for (int a = N; a > i; a--)
+		{
+			cout << " ";
+		}
+
+		for (int b = 1; b < i * 2; b++)
+		{
+			cout << "*";
+		}
+
+		cout << endl;
+	}
+}
+
+int main()
+{
+	int N;
+	cin >> N;
+	Pyramid(N);
+
+	return 0;
+}
