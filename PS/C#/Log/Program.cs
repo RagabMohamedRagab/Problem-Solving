206854fafbd3ec231e00ca185f83c13120fd87d3
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-07-24 19:59:21 +0300

    Log

---
+++ PS/C#/Log/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Log
+{
+     internal class Program{
+        public static int counter=0;
+        public static void Log(long N){
+            if(N==1){
+                return;
+            }
+            counter++;
+            Log(N/2);
+        }
+        private static void Main(string []args){
+            long N=Int64.Parse(Console.ReadLine());
+            Log(N);
+            Console.WriteLine(counter);
+        }
+     }
+}
