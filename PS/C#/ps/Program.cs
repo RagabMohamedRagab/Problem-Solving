35f494fc9444acf2bcd8df99a440592ed85b8b73
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-07-31 10:06:23 +0300

    Add files via upload

---
+++ PS/C#/ps/Program.cs
@@ -0,0 +1,24 @@
+﻿// Happy Code
+// Time Complexity O(N)
+// Space Complexity O(N)
+using System;
+namespace Average{
+    public class Program{
+        public static double AVG(int []A,int N){
+            if(N<0){
+                return 0;
+            }
+            return A[N]+AVG(A,N-1);
+        }
+        private static void Main(string []args){
+            int N=Int32.Parse(Console.ReadLine());
+            string []Arr=Console.ReadLine().Split(' ');
+            int []A=new int[N];
+            for(int i=0;i<N;i++){
+                A[i]=Int32.Parse(Arr[i]);
+            }
+           double result=(AVG(A,N-1)/N);
+           Console.WriteLine(result.ToString("#.000000"));
+        }
+    }
+}
